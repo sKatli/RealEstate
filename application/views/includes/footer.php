e28f2a11c59a3a4f838857f446e3dcb72ea0e9f8
Katlego Mosiako <kat.mosiako@gmail.com>
2016-12-09 18:01:15 +0000

    estate.css

---
+++ application/views/includes/footer.php
@@ -0,0 +1,6 @@
+
+<script type="text/javascript" src="<?= base_url("bootstrap.min.js"); ?>"></script>
+<script type="text/javascript" src="<?= base_url("jquery-3.1.1.min.js"); ?>"></script>
+<script type="text/javascript" src="<?= base_url("estate.js"); ?>"></script>
+</body>
+</html>
